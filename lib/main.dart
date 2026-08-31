0d90ea1c7415f5ae1ca0865537d9f5b2f5a78417
Romina <xxcontrexx@gmail.com>
2026-08-31 16:57:19 -0400

    Instalacion y prueba logger

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+
+final logger = Logger();
 
 void main() {
+  logger.i("Logger funcionando");
   runApp(const MyApp());
 }
 
